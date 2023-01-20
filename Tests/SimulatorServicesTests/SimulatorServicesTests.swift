34b827af014906bf25a92456628d31bbc0bd5e95
Leo Dion <leogdion@brightdigit.com>
2023-01-20 08:41:39 -0500

    setting up repo

---
+++ Tests/SimulatorServicesTests/SimulatorServicesTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SimulatorServices
+
+final class SimulatorServicesTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SimulatorServices().text, "Hello, World!")
+    }
+}
