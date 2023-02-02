71d24581862068be69fef4c2db2c9eb134c3e3c7
Leo Dion <leogdion@brightdigit.com>
2023-02-02 14:42:23 -0500

    fixing stream issue

--- Sources/SimulatorServices/Process/ProcessOutputHandleSet.swift
+++ Sources/SimulatorServices/Process/ProcessOutputHandleSet.swift
@@ -1,6 +1,37 @@
 import Foundation
 
+@available(macOS 10.15.4, *)
 internal struct ProcessOutputHandleSet {
-  internal let output: FileHandle
-  internal let error: FileHandle
+  private let output: FileHandle
+  private let error: FileHandle
+
+  private var _outputResult: Result<Data?, Error>?
+  private var _errorResult: Result<Data?, Error>?
+
+  internal var outputResult: Result<Data?, Error> {
+    guard let outputResult = _outputResult else {
+      preconditionFailure("Must call readToEnd first.")
+    }
+    return outputResult
+  }
+
+  internal var errorData: Data? {
+    guard let errorResult = _errorResult else {
+      preconditionFailure("Must call readToEnd first.")
+    }
+
+    return try? errorResult.get()
+  }
+
+  internal init(output: FileHandle, error: FileHandle) {
+    self.output = output
+    self.error = error
+    _outputResult = nil
+    _errorResult = nil
+  }
+
+  internal mutating func readToEnd() {
+    _outputResult = Result { try self.output.readToEnd() }
+    _errorResult = Result { try self.error.readToEnd() }
+  }
 }
