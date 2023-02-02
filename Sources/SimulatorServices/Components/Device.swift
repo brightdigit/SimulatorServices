7ddba2667d5ea2362ec903b283bd9fabbfa69de0
Leo Dion <leogdion@brightdigit.com>
2023-02-02 08:33:33 -0500

    revert TODO blanks

--- Sources/SimulatorServices/Components/Device.swift
+++ Sources/SimulatorServices/Components/Device.swift
@@ -2,22 +2,13 @@ import Foundation
 
 public struct Device: Decodable, Equatable {
   public let dataPath: Path // URL?
-
   public let dataPathSize: Int
-
   public let logPath: Path // URL?
-
   public let udid: UUID
-
   public let isAvailable: Bool
-
   public let logPathSize: Int?
-
   public let lastBootedAt: Date?
-
   public let deviceTypeIdentifier: DeviceTypeID // Identifier (Device)
-
   public let state: DeviceState // DeviceState
-
   public let name: String
 }
--- Sources/SimulatorServices/Components/DeviceType.swift
+++ Sources/SimulatorServices/Components/DeviceType.swift
@@ -1,19 +1,11 @@
 public struct DeviceType: Decodable, Equatable {
   public let productFamily: ProductFamily // deviceType
-
   public let bundlePath: Path // URL?
-
   public let maxRuntimeVersion: Int
-
   public let maxRuntimeVersionString: Version
-
   public let identifier: DeviceTypeID // identifier
-
   public let modelIdentifier: ModelID // model
-
   public let minRuntimeVersionString: Version
-
   public let minRuntimeVersion: Int
-
   public let name: String
 }
